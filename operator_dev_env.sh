013c12498c62c842ec1a9ba4227d22cf6e8fe950
JesseHoch <51460585+JesseHoch@users.noreply.github.com>
2022-10-19 13:12:13 -0400

    Updated script

---
+++ operator_dev_env.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+### Install Docker ###
+yum -y install docker
+usermod -a -G docker cloud_user
+su - cloud_user -c 'newgrp docker'
+systemctl enable docker.service
+systemctl start docker.service
+
+sleep 3
+
+### Install GO ###
+yum -y install go
+
+### Install kubectl ###
+curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.23.7/2022-06-29/bin/linux/amd64/kubectl
+chmod +x kubectl
+mv kubectl /usr/local/bin/
+
+sleep 3
+
+### Install kind ###
+go install sigs.k8s.io/kind@v0.16.0
+cp go/bin/kind /usr/local/bin/
+### Start kind Cluster and change context ###
+su - cloud_user -c 'kind create cluster --name operator-dev'
+su - cloud_user -c 'kubectl cluster-info --context kind-operator-dev'
+
+sleep 3
+
+### Install the Kubernetes Operator SDK ###
+export ARCH=$(case $(uname -m) in x86_64) echo -n amd64 ;; aarch64) echo -n arm64 ;; *) echo -n $(uname -m) ;; esac)
+export OS=$(uname | awk '{print tolower($0)}')
+export OPERATOR_SDK_DL_URL=https://github.com/operator-framework/operator-sdk/releases/download/v1.11.0
+curl -LO ${OPERATOR_SDK_DL_URL}/operator-sdk_${OS}_${ARCH}
+chmod +x operator-sdk_${OS}_${ARCH} && sudo mv operator-sdk_${OS}_${ARCH} /usr/local/bin/operator-sdk
+su - cloud_user -c '- [ ] operator-sdk olm install'
