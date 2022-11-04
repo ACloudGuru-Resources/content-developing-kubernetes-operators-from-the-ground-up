a5c579e3e7384746b9cf24d1f8ee4216ddd848ea
JesseHoch <51460585+JesseHoch@users.noreply.github.com>
2022-11-04 10:22:05 -0400

    Added a lab command file

--- lab_setup_2.sh
+++ lab_setup_2.sh
@@ -10,10 +10,17 @@ sleep 5
 ### Create the API and Controller ###
 operator-sdk create api --group cache --version v1alpha1 --kind Memcached --resource --controller
 
+sleep 5
+
 ### Add memcached_types.go file ###
-wget 
+wget https://raw.githubusercontent.com/ACloudGuru/content-developing-kubernetes-operators-from-the-ground-up/main/memcached_types.go
 mv memcached_types.go api/v1alpha1/
+
+sleep 5
+
 make generate
+
+sleep 3
 
 ## Generate CRD Manifests ###
 make manifests
