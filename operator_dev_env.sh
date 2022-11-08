61a24abb2a966c87c6292c4998e2aff7c055f029
JesseHoch <51460585+JesseHoch@users.noreply.github.com>
2022-11-07 23:50:00 -0500

    Update operator_dev_env.sh

--- operator_dev_env.sh
+++ operator_dev_env.sh
@@ -22,7 +22,7 @@ sleep 3
 ### Install the Kubernetes Operator SDK ###
 export ARCH=$(case $(uname -m) in x86_64) echo -n amd64 ;; aarch64) echo -n arm64 ;; *) echo -n $(uname -m) ;; esac)
 export OS=$(uname | awk '{print tolower($0)}')
-export OPERATOR_SDK_DL_URL=https://github.com/operator-framework/operator-sdk/releases/download/v1.25.0
+export OPERATOR_SDK_DL_URL=https://github.com/operator-framework/operator-sdk/releases/download/v1.25.1
 curl -LO ${OPERATOR_SDK_DL_URL}/operator-sdk_${OS}_${ARCH}
 chmod +x operator-sdk_${OS}_${ARCH} && sudo mv operator-sdk_${OS}_${ARCH} /usr/local/bin/operator-sdk
 su - cloud_user -c 'operator-sdk olm install'
