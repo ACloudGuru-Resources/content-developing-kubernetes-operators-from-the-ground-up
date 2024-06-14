1d32e316139066c52dfe4a893d648c0e12d7c96c
Marcos Monteiro <marcos-monteiro@pluralsight.com>
2024-06-14 17:30:22 +1000

    Update Go and Operator SDK

--- operator_dev_env.sh
+++ operator_dev_env.sh
@@ -10,8 +10,8 @@ systemctl start docker.service
 sleep 3
 
 ### Install GO ###
-wget https://go.dev/dl/go1.19.3.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.3.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.21.0.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.0.linux-amd64.tar.gz
 chmod +x /usr/local/go
 su - cloud_user -c 'export PATH=$PATH:/usr/local/go/bin'
 
@@ -25,7 +25,7 @@ sleep 3
 ### Install the Kubernetes Operator SDK ###
 export ARCH=$(case $(uname -m) in x86_64) echo -n amd64 ;; aarch64) echo -n arm64 ;; *) echo -n $(uname -m) ;; esac)
 export OS=$(uname | awk '{print tolower($0)}')
-export OPERATOR_SDK_DL_URL=https://github.com/operator-framework/operator-sdk/releases/download/v1.25.0
+export OPERATOR_SDK_DL_URL=https://github.com/operator-framework/operator-sdk/releases/download/v1.30.0
 curl -LO ${OPERATOR_SDK_DL_URL}/operator-sdk_${OS}_${ARCH}
 chmod +x operator-sdk_${OS}_${ARCH} && sudo mv operator-sdk_${OS}_${ARCH} /usr/local/bin/operator-sdk
 
